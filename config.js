49d0712f8669b440b2dd752c38e22e8137314cf0
Khufu737 <111590098+Khufu737@users.noreply.github.com>
2023-03-11 20:12:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -21,10 +21,10 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
+global.owner = ['201121968769'] //ur number
+global.ownernomer = "201121968769" //ur number
 global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
+global.premium = ['201121968769'] //ur premium number
 global.botname = 'Cheems Bot V5' //ur bot name
 global.ownername = "🦄Dream Guy Xeon"
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
